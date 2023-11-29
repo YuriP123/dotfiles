2048c860e4383ec8cca5a29a280a17302b87bafe
Miguel Yuri Pasamonte <76509924+YuriP123@users.noreply.github.com>
2023-11-29 10:46:09 -0800

    Add files via upload

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,10 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+
+---- Nvim discipline ----
+local discipline = require("yuri.discipline")
+discipline.cowboy()
+
+---- Basic Keymaps ---
